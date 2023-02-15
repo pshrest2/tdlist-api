614ca0fc2bb7be79f66df899bf0140d4b6867428
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-02-15 13:44:22 -0600

    implemented controller methods, seed, and react app

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,13 +1,28 @@
 class TodoListsController < ApplicationController
   def index
+    lists = TodoList.order(created_at: :desc)
+    render json: lists
   end
 
   def create
+    list = TodoList.create(todo_list_params)
+    render json: list
   end
 
   def update
+    list = TodoList.find(params[:id])
+    list.update(todo_list_params)
+    render json: list
   end
 
   def destroy
+    list = TodoList.find(params[:id])
+    list.destroy
+    head :no_content, status: :ok
+  end
+
+  private
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :done)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TodoList.create(title: "Schedule Meeting", done: false)
+TodoList.create(title: "Visit home", done: false)
+
