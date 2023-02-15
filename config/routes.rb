b7c90d9ad61aad8d1fffe452142c8eb4cb6b2fbf
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-02-15 11:36:35 -0600

    setup controller model and database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'todo_lists/index'
+  get 'todo_lists/create'
+  get 'todo_lists/update'
+  get 'todo_lists/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
