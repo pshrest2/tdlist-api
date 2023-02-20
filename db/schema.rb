4a0c7114dcc56cf4be93826f21426bb7bce25772
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-02-20 15:56:20 -0600

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_170217) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_215517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
     t.boolean "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
