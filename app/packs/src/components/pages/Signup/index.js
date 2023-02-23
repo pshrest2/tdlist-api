8e46fe3752a6ac4cd30f85f13fcecbc4c683a724
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-02-23 16:27:23 -0600

    let's not use password for this practice app

--- app/packs/src/components/pages/Signup/index.js
+++ app/packs/src/components/pages/Signup/index.js
@@ -16,14 +16,9 @@ const Signup = () => {
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password again" />
+        <Form.Group className="mb-3" controlId="formBasicPhone">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="text" placeholder="Enter US phone number" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
